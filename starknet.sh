50e0efe506b7131d9e1dacfcd330aac99c5a0b44
mrbnd <66267303+mrbnd@users.noreply.github.com>
2022-06-14 00:15:35 +0300

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -9,8 +9,8 @@ echo 'Ваш ключ: ' $ALCHEMY_KEY
 sleep 1
 echo 'export ALCHEMY_KEY='$ALCHEMY_KEY >> $HOME/.bash_profile
 
-#sudo apt update -y &>/dev/null
-#sudo apt install git docker docker-compose -y &>/dev/null
-#git clone https://github.com/mrbnd/starkNet-docker &>/dev/null
+sudo apt update -y &>/dev/null
+sudo apt install git docker docker-compose -y &>/dev/null
+git clone https://github.com/mrbnd/starkNet-docker &>/dev/null
 docker swarm init
 docker stack deploy --compose-file="$HOME/starkNet-docker/docker-compose.yml" stack
